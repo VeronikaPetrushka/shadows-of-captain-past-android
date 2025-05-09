d64cddd8da68834eaf9739591a590042593be73b
Veronika <veronikapetrushka@gmail.com>
2025-05-09 03:48:43 +0200

    icons

--- android/app/src/main/java/com/shadowOfCaptainPast/MainActivity.kt
+++ android/app/src/main/java/com/shadowOfCaptainPast/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.shadowsofcaptainpast
+package com.shadowOfCaptainPast
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ShadowsOfCaptainPast"
+  override fun getMainComponentName(): String = "Shadow of Captain Past"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/shadowOfCaptainPast/MainApplication.kt
+++ android/app/src/main/java/com/shadowOfCaptainPast/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.shadowsofcaptainpast
+package com.shadowOfCaptainPast
 
 import android.app.Application
 import com.facebook.react.PackageList
